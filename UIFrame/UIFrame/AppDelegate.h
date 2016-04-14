711f959f0e186334b31e4b8acf08c37d98399709
LanceZH <912796116@qq.com>
2016-04-14 09:07:22 +0800

    首次提交工程框架

---
+++ UIFrame/UIFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIFrame
+//
+//  Created by LanceZH on 16/4/13.
+//  Copyright © 2016年 LanceZH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
